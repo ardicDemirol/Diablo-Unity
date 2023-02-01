e4c14f66b4d7a9dc38d575a0862cf8755ae6d538
ArdicDemirol <ardc.dmrol@gmail.com>
2023-02-01 22:43:54 +0300

    levelUp VFX added

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
@@ -11,6 +12,12 @@ public class LevelManager : MonoBehaviour
     private int expToNextLevel;
 
     public int GetLevel { get { return level + 1; } }
+
+    public Image expBar;
+    public Text levelText;
+
+    public GameObject levelUpVFX;
+    private Transform player;
 
     private void Awake()
     {
@@ -27,15 +34,23 @@ public class LevelManager : MonoBehaviour
         level = 0;
         currentExp= 0;
         expToNextLevel = 100;
+        expBar.fillAmount = 0;
+        UpdateLevelText();
+        player = GameObject.Find("Player").gameObject.transform;
     }
 
     public void AddExp(int amount)
     {
         currentExp += amount;
+        expBar.fillAmount = (float)currentExp/expToNextLevel;
         if (currentExp > expToNextLevel)
         {
             level++;
+            GameObject levelUpVFXClone = Instantiate(levelUpVFX,player.position,Quaternion.identity);
+            levelUpVFXClone.transform.SetParent(player);
+            UpdateLevelText();
             currentExp -= expToNextLevel;
+            expBar.fillAmount = 0f;
         }
     }
 
@@ -48,11 +63,16 @@ public class LevelManager : MonoBehaviour
         EnemyHealth.onDeath -= AddExp;
     }
 
+    void UpdateLevelText()
+    {
+        levelText.text = GetLevel.ToString();
+    }
+
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Z)) 
         {
-            AddExp(100);
+            AddExp(20);
             Debug.Log(level);
 
         }
